c19d820ac1fe034320ed9597bceaf417391df933
Flávio Dutra <flaviomdutra@outlook.com>
2024-06-19 07:44:47 -0300

    feat: DDD structure

---
+++ domain/src/main/java/com/fullcycle/subscription/domain/DomainEvent.java
@@ -0,0 +1,11 @@
+package com.fullcycle.subscription.domain;
+
+import java.time.Instant;
+
+public interface DomainEvent extends AssertionConcern {
+    Instant occurredOn();
+
+    String aggregateId();
+
+    String aggregateType();
+}
