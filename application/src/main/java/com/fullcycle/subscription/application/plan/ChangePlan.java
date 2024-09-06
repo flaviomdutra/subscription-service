b057e6b108325729d6ef89a51f287118fb5fc9e3
Flávio Dutra <flaviomdutra@outlook.com>
2024-09-06 16:08:34 -0300

    feat: Pna

---
+++ application/src/main/java/com/fullcycle/subscription/application/plan/ChangePlan.java
@@ -0,0 +1,25 @@
+package com.fullcycle.subscription.application.plan;
+
+import com.fullcycle.subscription.application.UseCase;
+import com.fullcycle.subscription.domain.plan.PlanId;
+
+public abstract class ChangePlan extends UseCase<ChangePlan.Input, ChangePlan.Output> {
+
+    public interface Input {
+        Long planId();
+
+        String name();
+
+        String description();
+
+        Double price();
+
+        String currency();
+
+        Boolean active();
+    }
+
+    public interface Output {
+        PlanId planId();
+    }
+}
