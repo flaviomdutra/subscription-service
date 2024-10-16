75bb61c945b79d224185240fbf370c0e7c68e391
Flávio Dutra <flaviomdutra@outlook.com>
2024-10-16 13:57:10 -0300

    feat: Plan Tests

--- infrastructure/src/test/java/com/fullcycle/subscription/infrastructure/gateway/repository/PlanJdbcRepositoryTest.java
+++ infrastructure/src/test/java/com/fullcycle/subscription/infrastructure/gateway/repository/PlanJdbcRepositoryTest.java
@@ -1,17 +1,16 @@
 package com.fullcycle.subscription.infrastructure.gateway.repository;
 
 import com.fullcycle.subscription.AbstractRepositoryTest;
-import com.fullcycle.subscription.domain.account.AccountId;
-import com.fullcycle.subscription.domain.account.idp.UserId;
-import com.fullcycle.subscription.domain.person.Address;
-import com.fullcycle.subscription.domain.person.Document;
-import com.fullcycle.subscription.domain.person.Email;
-import com.fullcycle.subscription.domain.person.Name;
+import com.fullcycle.subscription.domain.money.Money;
+import com.fullcycle.subscription.domain.plan.PlanId;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.jdbc.Sql;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class PlanJdbcRepositoryTest extends AbstractRepositoryTest {
     @Test
@@ -20,29 +19,34 @@ class PlanJdbcRepositoryTest extends AbstractRepositoryTest {
     }
 
     @Test
-    @Sql({"classpath:/sql/accounts/seed-account-johndoe.sql"})
-    public void givenPersistedAccount_whenQueriesSuccessfully_shouldReturnIt() {
+    @Sql({"classpath:/sql/plans/seed-plan-master.sql"})
+    public void givenPersistedPlan_whenQueriesSuccessfully_shouldReturnIt() {
         // given
-        Assertions.assertEquals(1, countAccounts());
+        Assertions.assertEquals(1, countPlans());
 
-        var expectedId = new AccountId("033c7d9eb3cc4eb7840b942fa2194cab");
+        var expectedId = new PlanId(1L);
         var expectedVersion = 1;
-        var expectedUserId = new UserId("a8b3cf5a-5f81-4822-9ee8-89e768f6095c");
-        var expectedEmail = new Email("john@gmail.com");
-        var expectedName = new Name("John", "Doe");
-        var expectedDocument = new Document.Cpf("12312312332");
-        var expectedAddress = new Address("12332123", "1", "Casa 1", "BR");
+        var expectedName = "Master";
+        var expectedDescription = "O plano mais custo benefício";
+        var expectedActive = false;
+        var expectedPrice = new Money("BRL", 20.0);
+        var expectedCreatedAt = Instant.parse("2024-04-28T10:57:11.111Z");
+        var expectedUpdatedAt = Instant.parse("2024-04-28T10:58:11.111Z");
+        var expectedDeletedAt = Instant.parse("2024-04-28T10:59:11.111Z");
+
 
         // when
-        var actualAccount = this.accountRepository().accountOfId(expectedId).get();
+        var actualPlan = this.planRepository().planOfId(expectedId).get();
 
         // then
-        assertEquals(expectedId, actualAccount.id());
-        assertEquals(expectedVersion, actualAccount.version());
-        assertEquals(expectedUserId, actualAccount.userId());
-        assertEquals(expectedEmail, actualAccount.email());
-        assertEquals(expectedName, actualAccount.name());
-        assertEquals(expectedDocument, actualAccount.document());
-        assertEquals(expectedAddress, actualAccount.billingAddress());
+        assertEquals(expectedId, actualPlan.id());
+        assertEquals(expectedVersion, actualPlan.version());
+        assertEquals(expectedName, actualPlan.name());
+        assertEquals(expectedDescription, actualPlan.description());
+        assertEquals(expectedActive, actualPlan.active());
+        assertEquals(expectedPrice, actualPlan.price());
+        assertEquals(expectedCreatedAt, actualPlan.createdAt());
+        assertEquals(expectedUpdatedAt, actualPlan.updatedAt());
+        assertEquals(expectedDeletedAt, actualPlan.deletedAt());
     }
 }
